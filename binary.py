60983d4f70990a68158dfb15b9249cf1d7f877df
gitmapd <50059062+gitmapd@users.noreply.github.com>
2022-06-19 22:47:25 +0100

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def Str2IPV4(s):
+  return np.array(s.split('.'), dtype=np.uint8)
+
+def NetAddress(ipadd, netmask):
+  return Str2IPV4(ipadd) & Str2IPV4(netmask)
+
+def IPV42Str(ipv4):
+  return '.'.join(ipv4.astype(str))
+
+def WildCard(subnetmask):
+    return ~Str2IPV4(subnetmask)
+
+def WildCardMask(subnetmask,netaddress):
+    return ~Str2IPV4(subnetmask) | Str2IPV4(netaddress)
+
+
+
